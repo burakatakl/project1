dac812b4ad126b9d94f1069956f6a6ff966d9680
burakatakl <149960490+burakatakl@users.noreply.github.com>
2023-11-18 15:28:02 +0100

    I opned new header of insertationSort.h and i try to explain all codes on this file

--- insertationSort.h
+++ insertationSort.h
@@ -1,6 +1,28 @@
+#include <iostream>
+#include <vector>
+
 
 #ifndef PROJECT1_INSERTATIONSORT_H
 #define PROJECT1_INSERTATIONSORT_H
 
+void insertationSort ( std::vector<int> &arr){
+    int n = arr.size(); //here i am getting size of vector
+    for(int i=1; i<n; i++){
 
-#endif //PROJECT1_INSERTATIONSORT_H
+        int inser= arr[i];
+        int j=i-1;
+        while (j >= 0 && arr[j]> inser){
+
+    arr[j+1]= arr[j];// here i i shifted the elements right
+    j--;// here i move the previus element the left
+}
+        arr[j +1] = inser;// last one in here i putted correct form the elements
+    }
+
+
+}
+
+
+
+#endif //PROJECT1_INSERTATIONSORT_Hn ){
+
